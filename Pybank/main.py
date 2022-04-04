fad36840ac7a524b9143aeb0f443bc124abf02ab
hanniecodes <hannahgallen@gmail.com>
2022-04-04 18:02:52 -0400

    final version of homework

--- Pybank/main.py
+++ Pybank/main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #py_budget
 print("Financial Analysis")
 print("----------------------------------")
@@ -74,3 +75,81 @@ f.write("Total:$"+ str(total))
 f.write("Average Change:$"+str(round(totalchange_mean,2)))
 f.write("'Greatest Increase in Profits:"+str(increasemonth)+"  $"+str(increase))
 f.write("'Greatest Decrease in Profits:"+str(decreasemonth)+"  $"+str(decrease))
+=======
+#py_budget
+print("Financial Analysis")
+print("----------------------------------")
+
+import os 
+import csv
+
+
+csvpath ="./Resources/budget_data.csv"
+
+with open(csvpath, encoding='utf') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    #I coded the lines because line count is the same as number of months presumably 
+   
+    #skip first row 
+    next(csvreader, None)
+    
+#variables
+    linecount=0
+    total=0
+    totalchange_mean=0
+    increase=0
+    decrease=0
+    total_change=0 
+    change=0 
+    previousrow=0
+    current=0
+    #empty month 
+    month=0
+    increasemonth=0
+    decreasemonyh=0
+    #storing things in a list
+    oc=[]
+
+    for row in csvreader:
+        #date.append(row[0])
+        linecount+=1
+        total=total+int(row[1])
+        #setting a special case for row 2 two becase our change for row 2 is 0 
+        #below code for calc average change
+        if csvreader.line_num==2:
+            previousrow=int(row[1])
+        change=int(row[1])-previousrow
+        total_change=total_change+change
+        previousrow=int(row[1])
+
+        # #for greatest increase
+        if (increase<int(row[1])):
+            increase=int(row[1])
+            increasemonth=row[0]
+            
+        # #for greatest decrease
+        if(decrease>int(row[1])):
+            decrease=int(row[1])
+            decreasemonth=row[0]
+
+    totalchange_mean=total_change/linecount
+
+    print(f'Total months:{linecount}')
+    print(f'Total:${total}')
+    print(f'Average Change:${round(totalchange_mean,2)}')
+   
+    print(f'Greatest Increase in Profits:{increasemonth}(${increase})')
+    print(f'Greatest Decrease in Profits:{decreasemonth}(${decrease})')
+#Don't forget output
+
+f = open("analysis.txt", "w")
+f = open("analysis.txt", "a")
+f.write("Financial Analysis")
+f.write("-------------------------")
+f.write("Total months:" + str(linecount))
+f.write("-------------------------")
+f.write("Total:$"+ str(total))
+f.write("Average Change:$"+str(round(totalchange_mean,2)))
+f.write("'Greatest Increase in Profits:"+str(increasemonth)+"  $"+str(increase))
+f.write("'Greatest Decrease in Profits:"+str(decreasemonth)+"  $"+str(decrease))
+>>>>>>> b6673d509c7e7f8793e7411ef5797a1dfed3d6ad
--- Pypoll/main.py
+++ Pypoll/main.py
@@ -1,3 +1,79 @@
+<<<<<<< HEAD
+#Pypoll
+print("Election Results")
+print("-------------------------")
+from ast import NotIn
+import os 
+import csv
+
+from matplotlib.collections import LineCollection
+
+csvpath ="./Resources/election_data.csv"
+
+with open(csvpath, encoding='utf') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    #skip first row
+    next(csvreader, None)
+  
+    #set variables
+    linecount=0
+    totalvotes=0
+    c_list=[]
+    v_list=[]
+    canditate=0
+    votepercent=0
+    winner=0
+    winnerv_list=0
+  
+    
+#counting total and getting list of candidates 
+    for row in csvreader:
+        totalvotes+=1
+ 
+    #list of all of the individual candidates in list within the voter list then printing them
+        if (row [2] not in c_list):
+            c_list.append(row[2])
+            v_list.append(0)
+        c_index=c_list.index(row[2])
+        v_list[c_index]+=1
+print(f'Total Votes:{totalvotes}')
+print("-------------------------")
+
+#printing the winner and getting alist of all the cadidates
+for x in range(len(c_list)):
+        voteper=round((v_list[x]/totalvotes)*100,3)
+        #printing the list of candidates
+        print(f"{c_list[x]}:{voteper}% ({v_list[x]})")
+        if winnerv_list<v_list[x]:
+            winnerv_list=v_list[x]
+            winner=c_list[x]
+
+print("-------------------------")
+print(f"Winner: {winner}")
+print("-------------------------")
+
+
+
+f = open("analysis.txt", "w")
+f = open("analysis.txt", "a")
+f.write("Election Results")
+f.write("-------------------------")
+f.write("Total votes:" + str(totalvotes))
+f.write("-------------------------")
+
+for x in range(len(c_list)):
+    voteper = round((v_list[x]/totalvotes)*100,3)
+    f.write("\n" + str(c_list[x]) +" : " + str(voteper) 
+                + "% ("+ str(v_list[x]) + ")")
+
+f.write("-------------------------")
+# print(winner)
+f.write("Winner: " + str(winner))
+f.write("-------------------------")
+
+
+
+=======
 #Pypoll
 print("Election Results")
 print("-------------------------")
@@ -71,3 +147,4 @@ f.write("-------------------------")
 
 
 
+>>>>>>> b6673d509c7e7f8793e7411ef5797a1dfed3d6ad
